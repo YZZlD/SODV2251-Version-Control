df7b1fa91d98c2b4d46e7e64c55a8e052ded602f
YZZlD <max.verhulst@hotmail.com>
2026-05-09 15:38:14 -0600

    feat: navigation ui added for returning to home from map page

--- dev-peer-finder/app/map.tsx
+++ dev-peer-finder/app/map.tsx
@@ -1,11 +1,22 @@
+import { useRouter, Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function Map() {
+
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.navigationLinkContainer}>
+            <Link href="/" asChild>
+                <Pressable style={styles.button}>
+                    <Text>Home</Text>
+                </Pressable>
+            </Link>
+        </View>
       <Text>This is the Map</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +28,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: "#5e5ef8",
+    borderWidth: 2,
+    borderColor: "#000000",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  navigationLinkContainer: {
+    position: "absolute",
+    top: 60,
+    left: 20,
+  }
 });
