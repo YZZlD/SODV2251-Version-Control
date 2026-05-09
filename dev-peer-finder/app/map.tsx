f364769ccfae977d50a9b7b4ba917b3a31b2a02c
YZZlD <max.verhulst@hotmail.com>
2026-05-09 16:23:43 -0600

    fix: cleaned unnecessary imports

--- dev-peer-finder/app/map.tsx
+++ dev-peer-finder/app/map.tsx
@@ -1,7 +1,6 @@
-import { useRouter, Link } from 'expo-router';
+import { useRouter,} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function Map() {
 
